a75471ab0dedc64b3d4f41d0cc22a7dc3867bb4a
AWDPL <atomwaffendivision55@protonmail.com>
2026-01-01 22:20:22 +0100

    Add sample training videos data

---
+++ videos.js
@@ -0,0 +1,12 @@
+[
+  {
+    "id": "dQw4w9WgXcQ",
+    "title": "Przykładowy film treningowy 1",
+    "description": "Opis filmu — wskazówki techniczne."
+  },
+  {
+    "id": "9bZkp7q19f0",
+    "title": "Przykładowy film treningowy 2",
+    "description": "Krótkie omówienie ćwiczeń."
+  }
+]
